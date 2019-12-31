50710211b1cb2f46d5ff8a3a0c12b87eeeb2a788
Snowmen <1211722058@qq.com>
2019-12-31 15:24:30 +0800

    创建项目

---
+++ CourseManager/CourseManager/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CourseManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "你的应用程序说明页。";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "你的联系方式页。";
+
+            return View();
+        }
+    }
+}
